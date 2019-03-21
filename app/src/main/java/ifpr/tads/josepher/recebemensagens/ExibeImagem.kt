c20699b1c7c8f2003aad416e01f1cd0d6e1e5c15
Josepher José Castro da Silva <josephercastrosilva@gmail.com>
2019-03-21 03:04:33 +0000

    1ª Interface - não inflada

---
+++ app/src/main/java/ifpr/tads/josepher/recebemensagens/ExibeImagem.kt
@@ -0,0 +1,12 @@
+package ifpr.tads.josepher.recebemensagens
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExibeImagem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exibe_imagem)
+    }
+}
